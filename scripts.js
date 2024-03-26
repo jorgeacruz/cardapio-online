69ad56fee68fad1d211d0d7efe110986f2cb496d
Jorge Cruz <j.alexcruz@gmail.com>
2024-03-26 09:59:42 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
+console.log('testeee')
+
 const menu = document.getElementById('menu');
+const cartBtn = document.getElementById("cartBtn");
+const cartModal = document.getElementById("cartModal");
+const cartItemsContainer = document.getElementById('cart-items');
+const cartTotal = document.getElementById('cart-total');
+const checkoutBtn = document.getElementById('checkout-btn');
+const closeModalBtn = document.getElementById('close-modal-btn');
+const cartCounter = document.getElementById('cart-counter');
+const addressInput = document.getElementById('address');
+const addressWarn = document.getElementById('address-warn');
+
+
+// Adicionando um event listener ao botão
+cartBtn.addEventListener("click", function() {
+    // Alterando o estilo de exibição do modal do carrinho para "flex" quando o botão é clicado
+    cartModal.style.display = "flex";
+});
+
